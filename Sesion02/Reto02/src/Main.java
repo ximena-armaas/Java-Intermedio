446852f69b40a559d0a1c53d642eb054ca6f58c8
ximena-armaas <114454320+ximena-armaas@users.noreply.github.com>
2025-06-17 15:52:44 -0600

    Reto02 update

--- Sesion02/Reto02/src/Main.java
+++ Sesion02/Reto02/src/Main.java
@@ -1,7 +1,18 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Iniciando simulación");
+        RecursoMedico salaUrgencias = new RecursoMedico("Sala de urgencias");
 
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.submit(new PersonalMedico("Dra. Armas",salaUrgencias));
+        executor.submit(new PersonalMedico("Dr. Juan", salaUrgencias));
+        executor.submit(new PersonalMedico("Dr. Pedrito", salaUrgencias));
+
+        executor.shutdown();
     }
 }
--- Sesion02/Reto02/src/PersonalMedico.java
+++ Sesion02/Reto02/src/PersonalMedico.java
@@ -1,9 +1,18 @@
-public class PersonalMedico {
+public class PersonalMedico implements Runnable {
     public String nombre;
-
-    public PersonalMedico(String nombre){
+    public RecursoMedico recuso;
+    public PersonalMedico(String nombre, RecursoMedico recuso){
+        this.recuso=recuso;
         this.nombre=nombre;
     }
 
-    
+    @Override
+    public void run(){
+        try {
+            recuso.usar(nombre);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
